539453e75e91628d665ef745076fe93c70f93225
Paul-Emmanuel DOS SANTOS FACAO <pedsf.fullstack@gmail.com>
2022-07-24 07:32:39 +0000

    Get server config

--- multipleServerInventory.sh
+++ multipleServerInventory.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 # you must define SSHPASS variable with your user password
 
-REMOTE_USER="$HOME/.ssh/remoteuser"
+
+REMOTE_USER_FILE="$HOME/.ssh/remoteuser"
+REMOTE_USER=$(cat "$REMOTE_USER_FILE")
 REMOTE_PASS="$HOME/.ssh/remotepass"
 REMOTE_SERVERS="$HOME/bash-practice/serversList.txt"
 SSH_OPTIONS="sshpass -f $REMOTE_PASS ssh -n -o StrictHostKeyChecking=No -o PubkeyAuthentication=No"
+OUTPUT_FILE="servers_config.csv"
 EXIT_NO_USER_FILE=1
 EXIT_NO_PASSWORD_FILE=2
 EXIT_NO_SERVER_FILE=3
 
+######################################################################### BANNER
 display_separator(){
    printf "#%0.s" $(seq 1 $(tput cols))
    printf "\n"
@@ -28,8 +32,9 @@ display_banner(){
 
 }
 
+################################################################### CHECK CONFIG
 check_remoteuser(){
-   if [[ ! -e "$REMOTE_USER" ]]
+   if [[ ! -e "$REMOTE_USER_FILE" ]]
    then
       display_banner "Please store your login in $REMOTE_USER file"
       exit $EXIT_NO_USER_FILE
@@ -52,23 +57,51 @@ check_remoteservers(){
    fi
 }
 
+####################################################################### GET INFO
+get_ostype(){
+   server=$1
+   osType=$($SSH_OPTIONS $REMOTE_USER@$server "cat /etc/os-release" | grep -w "NAME"|awk -F '[" ]' '{print $2}')
+   echo "$osType"
+}
+
+get_osversion(){
+   server=$1
+   osType=$2
+
+   case $osType in
+   "Ubuntu")
+       osVersion=$($SSH_OPTIONS $REMOTE_USER@$server "cat /etc/os-release" | grep -w "VERSION_ID" | awk -F "VERSION_ID=" '{print $2}')
+      ;;
+   "Fedora")
+      osVersion=$($SSH_OPTIONS $REMOTE_USER@$server "cat /etc/os-release" | grep -w "VERSION_ID" | awk -F '=' '{print $2}')
+      ;;
+   *)
+      osVersion="Not found"
+   esac 
+   echo "$osVersion"
+}
+
+get_arctype(){
+   server=$1
+   echo $($SSH_OPTIONS $REMOTE_USER@$server "uname -p")
+}
+
+get_cputype(){
+   server=$1
+   echo $($SSH_OPTIONS $REMOTE_USER@$server "cat /proc/cpuinfo | grep 'model name' | cut -d':' -f2 | head -1")
+}
+
+###################################################################### PULL-INFO
 pull_info(){
-   echo "Pull info"
-   userLogin=$( cat $REMOTE_USER)
+    echo "OS type,OS version,Architecture Type,CPU" > $OUTPUT_FILE
    while read server
    do
       echo "Work on server $server"
-      osType=$($SSH_OPTIONS $userLogin@$server "cat /etc/os-release" | grep -w "NAME"|awk -F "NAME=" '{print $2}'|tr '"' ' ')
-      echo "$osType" | grep -i "ubuntu" 1>/dev/null 2>&1
-      if [[ $? -eq 0 ]]
-      then
-         osVersion=$($SSH_OPTIONS $userLogin@$server "cat /etc/os-release" | grep -w "VERSION_ID" | awk -F "VERSION_ID=" '{print $2}')
-      else
-         osVersion=$($SSH_OPTIONS $userLogin@$server "cat /etc/redhat-release" | awk -F "release" | awk '{print $1}')
-      fi
-      arcType=$($SSH_OPTIONS $userLogin@$server "uname -p")
-      cpuType=$($SSH_OPTIONS $userLogin@$server "cat /proc/cpuinfo | grep 'model name' | cut -d':' -f2 | head -1")
-      echo "OS type: $osType\n OS version: $osVersion\n Architecture Type: $arcType\n CPU: $cpuType"
+      osType=$(get_ostype $server)
+      osVersion=$(get_osversion $server $osType)
+      arcType=$(get_arctype $server)
+      cpuType=$(get_cputype $server)
+      echo "$osType,$osVersion,$arcType,$cpuType" >> $OUTPUT_FILE
    done < $REMOTE_SERVERS
 }
 
